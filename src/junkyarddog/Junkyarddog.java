af2e135b5e56f8fdc8c9ef02bddb840fafa89fe1
rjackson <richgjackson@yahoo.co.uk>
2015-10-02 11:03:05 +0100

    sql

--- src/junkyarddog/Junkyarddog.java
+++ src/junkyarddog/Junkyarddog.java
@@ -82,7 +82,7 @@ public class Junkyarddog {
             sb.append("gender = \'" + gender.get(rand.nextInt(3))+"\' ,");
             sb.append("medication = \'" + buildMedicationJson()+"\'");
             
-            sb.append(" where  id in (select id from process_source_new where patient_id is not null order by random() limit 50) ");
+            sb.append(" where  id in (select id from process_source_new where patient_id is null order by random() limit 50) ");
             sb.append(";\n");
                                                                                            
             
